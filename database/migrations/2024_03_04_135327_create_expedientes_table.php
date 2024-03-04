f25288e21d25118ea006cb70bcc14059f0c90bac
royantoni <ferroafataroyantoni@gmail.com>
2024-03-04 09:28:33 -0500

    Se modifico la base de datos

---
+++ database/migrations/2024_03_04_135327_create_expedientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expedientes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('estadoe')->default(0);
+            $table->text('observacione')->nullable();
+            $table->boolean('visible')->default(false);
+            $table->foreignId('denuncia_id')->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expedientes');
+    }
+};
